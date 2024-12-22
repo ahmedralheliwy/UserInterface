5a832d7d6418279e010fac61d1a68bd89b254ffa
ahmedralheliwy <ahmed.alheliwy0.99@gmail.com>
2024-12-22 07:08:47 +0200

    C3: complete user creation form

--- UserInterface/accounts/views.py
+++ UserInterface/accounts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method=='POST':
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+            #Or user=form.save() from contrib.auth.login
+            #And login(request, user)
+            #Then return redirect('home')
+    else:
+        form=UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form':form})
 
 def login(request):
     return render(request, 'accounts/login.html')
